febf5c76e4214511806d38a5a83da7f99b332f52
Santhosh071 <94433539+Santhosh071@users.noreply.github.com>
2023-12-31 22:40:11 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,6 +1,11 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
-};
+    apiKey: "AIzaSyA-tgtxkNjeoLgKS_djiaIDDiglSnwhbpA",
+    authDomain: "blogging-b8df8.firebaseapp.com",
+    projectId: "blogging-b8df8",
+    storageBucket: "blogging-b8df8.appspot.com",
+    messagingSenderId: "271560195896",
+    appId: "1:271560195896:web:30cf2d93c2859b903a0bcd"
+ };
 
 firebase.initializeApp(firebaseConfig);
 
